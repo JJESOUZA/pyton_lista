3f8f97aac39e15ebe1620e470d54034bbbc4a584
JESSICA  SOUZA DA SILVA <0220832221021@fatectq.local>
2024-03-07 11:10:44 -0300

    pyton commit

---
+++ aula1.py
@@ -0,0 +1,36 @@
+#lista = [10,20,30,40,50]
+#print (lista)
+#lista = [0] *10
+#print (lista)
+#lista = lista + [5]*2
+#print (lista)
+
+#lista = [10,20,30,40,50]
+#print (lista)
+#print (f'Tamanho da lista = {len(lista)}')
+#print (f'Maior elemento da lista = {max(lista)}')
+#print (f'Menor da lista = {min(lista)}')
+
+#del apaga o elemento da referida posicao
+#del lista [3]
+#print(f'a nova lista retirando o elemento que estava na 3 posicao = {lista}') 
+
+#cls para limpar o terminal.
+
+#i é elemento ex:numero
+
+for i in range (2,7):
+    print (i, end=' ')
+print () 
+
+for i in range (1,9,2):
+    print (i, end=' ')
+print ()
+
+lista=[10,20,30,40,50]
+for i in lista:
+    print (i, end=' ')
+print ()
+
+for i in range (len(lista)):
+    print (lista [i], end=' ')
