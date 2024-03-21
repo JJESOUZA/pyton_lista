4b621bfc26b76f6ab86581b36492f7f735b13021
JESSICA  SOUZA DA SILVA <0220832221021@fatectq.local>
2024-03-21 11:54:34 -0300

    aula2

---
+++ aula2.py
@@ -0,0 +1,50 @@
+#random biblioteca, time 
+#numero=random.randint (1,100)
+#tempo=time.time () 
+#tentativas= int.input()
+#tentativas
+
+#random biblioteca, 
+#time 
+#numero=random.randint (1,100)
+#tempo=time.time () 
+#tentativas=0 
+#while true=
+#tentativas +=1
+#papite=int (input ("qual sua sugestão"))
+#if palpite <> numero
+#print("tente outra vez")
+#else:
+#tempofinal=time.time()
+#tempototal=time.time ()
+#tempototal=tempofina-tempo 
+#"print (parabens..{numero}em {tentativa} tentativas num tempo de {tentativas} )""
+
+import time
+import random
+numero=  random.randint(1,100)
+
+tempo_start= time.time ()
+
+resposta = 0
+tentativa = 0
+print("Palpite gerado!")
+
+chute= 0
+while chute is not numero:
+    tentativa +=1
+    chute = int(input("Qual seu chute: "))
+    if chute > numero:
+        print("Errou! É um valor menor que ", chute)
+    elif chute < numero:
+        print("Errou! É um valor maior que ", chute)
+    else:
+        print(f"Parabéns! O número gerado foi ", {numero}, 
+        "Acertou em ",{tentativa}," tentativas!")
+        break
+
+tempofinal= time.time()
+tempototal= tempofinal - tempo_start
+print (f" SEU TEMPO FINAL FOI: em {numero:.2f} segundos ")
+
+
